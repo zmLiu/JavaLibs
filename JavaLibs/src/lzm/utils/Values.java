4feb701726d184698e7e73b66800435547f41953
zmLiu <470301594@qq.com>
2014-05-22 10:27:11 +0800

    修改一些值

--- JavaLibs/src/lzm/utils/Values.java
+++ JavaLibs/src/lzm/utils/Values.java
@@ -2,13 +2,16 @@ package lzm.utils;
 
 public class Values {
 
-	/** 7天的秒数 */
-	public static final int _7DaySeconds = 7 * 24 * 60 * 60;
-
 	/** 60秒 */
 	public static final int _60Seconds = 60;
 	
 	/** 3小时的秒数 */
-	public static final int _3HourSeconds = 3 * 60 *60;
+	public static final int _3HourSeconds = 10800;
+	
+	/** 1天的秒数 */
+	public static final int _1DaySeconds = 86400;
+	
+	/** 7天的秒数 */
+	public static final int _7DaySeconds = 604800;
 
 }
