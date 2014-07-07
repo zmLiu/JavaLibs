0222e12f2b7df69bfe5f996669818ed0472722cc
zmLiu <470301594@qq.com>
2014-07-07 10:58:50 +0800

    json支持中文转换为\uXXXX

--- JavaLibs/src/lzm/bo/BaseBo.java
+++ JavaLibs/src/lzm/bo/BaseBo.java
@@ -8,6 +8,7 @@ import lzm.utils.BeanUtil;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 
 public class BaseBo {
 	
@@ -82,5 +83,17 @@ public class BaseBo {
 	 * */
 	public String toJSONString(){
 		return JSON.toJSONString(this);
+	}
+	
+	/**
+	 * 获取对象的json字符串
+	 * @param converChinese 是否转换中文为\\uXXXX
+	 * */
+	public String toJSONString(boolean converChinese){
+		if(converChinese){
+			return JSON.toJSONString(this,SerializerFeature.BrowserCompatible);
+		}else{
+			return JSON.toJSONString(this);
+		}
 	}
 }
