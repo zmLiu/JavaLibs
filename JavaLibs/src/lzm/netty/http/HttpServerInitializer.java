520a5f113230da99d4ea58edee275569b304b56c
zmLiu <470301594@qq.com>
2014-08-24 21:46:34 +0800

    http handler修改

--- JavaLibs/src/lzm/netty/http/HttpServerInitializer.java
+++ JavaLibs/src/lzm/netty/http/HttpServerInitializer.java
@@ -4,8 +4,7 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.stream.ChunkedWriteHandler;
 import lzm.netty.http.handler.HttpServerHandler;
 
@@ -15,11 +14,7 @@ public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
 	protected void initChannel(SocketChannel ch) throws Exception {
 		ChannelPipeline pipeline = ch.pipeline();
 		
-//		pipeline.addLast("readTimeoutHandler", new ReadTimeoutHandler(HttpServerConfig.time_out));
-//     	pipeline.addLast("writeTimeoutHandler", new WriteTimeoutHandler(HttpServerConfig.time_out));
-		
-		pipeline.addLast("encoder", new HttpResponseEncoder());
-        pipeline.addLast("decoder", new HttpRequestDecoder());
+		pipeline.addLast("codec",new HttpServerCodec());
         pipeline.addLast("aggregator", new HttpObjectAggregator(65536));//用于将解析出来的数据封装成http对象，httprequest什么的
         pipeline.addLast("chunkedWriter", new ChunkedWriteHandler());
         
